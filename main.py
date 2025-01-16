75cc9c406f4c9d47e9c4b36c688990eef896070f
kasztan-nut <135378750+kasztan-nut@users.noreply.github.com>
2025-01-16 13:23:00 +0100

    Added more examples of input data

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from parser import parser
 
 if __name__ == '__main__':
     input_data = "今日は学校に行きます。 かっこいい。 やきゅう。 ハンバーガー。 しゃ。 しや。 きゃ。 きや。 ティ。 ファ。  デュ"
-    # incorrect_data = "focus"
-    incorrect_data = "っね"
+    incorrect_data = "focus"
+    incorrect_data_1 = "っね"
+    incorrect_data_2 = "ねゅ"
+    incorrect_data_3 = "ッフ"
 
     print(f"Japanese: {input_data}")
     romaji = parser.parse(input_data)
@@ -11,4 +13,16 @@ if __name__ == '__main__':
 
     print(f"Japanese: {incorrect_data}")
     wrong_romaji = parser.parse(incorrect_data)
+    print(f"Romaji: {wrong_romaji}")
+
+    print(f"Japanese: {incorrect_data_1}")
+    wrong_romaji = parser.parse(incorrect_data_1)
+    print(f"Romaji: {wrong_romaji}")
+
+    print(f"Japanese: {incorrect_data_2}")
+    wrong_romaji = parser.parse(incorrect_data_2)
+    print(f"Romaji: {wrong_romaji}")
+
+    print(f"Japanese: {incorrect_data_3}")
+    wrong_romaji = parser.parse(incorrect_data_3)
     print(f"Romaji: {wrong_romaji}")
